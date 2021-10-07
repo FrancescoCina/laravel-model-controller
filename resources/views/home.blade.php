a53b2f36846670c29f26f10531adbc47c8cd2c53
FrancescoCina <francesco.cina04@gmail.com>
2021-10-07 16:07:27 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title-page')
+    <h1 class="text-center my-4">Movies</h1>
+
+
+
+@endsection
+
+@section('content')
+    
+
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <ul class="nav fs-3">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Movies</a>
+                </li>
+              </ul>
+          </div>
+      </nav>
+</header>
